5d95faa04bdb9641b8a72dd6b57fbd6253d8b5e3
Filippo Sciorelli <10899733@polimi.it>
2026-03-17 15:52:53 +0100

    [ShamanVisitor] Implement logic
    implemented logic in ShamanVisitor class

--- src/main/java/it/polimi/gc06/mesos/Model/ShamanVisitor.java
+++ src/main/java/it/polimi/gc06/mesos/Model/ShamanVisitor.java
@@ -1,4 +1,65 @@
 package it.polimi.gc06.mesos.Model;
 
-public class ShamanVisitor {
+public class ShamanVisitor implements TribeCardVisitor{
+
+    private final Player player;
+
+    public ShamanVisitor(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public void visit(RitualEvent ritual) {
+
+    }
+
+    @Override
+    public void visit(SustenanceEvent sustenance) {
+
+    }
+
+    @Override
+    public void visit(HuntEvent hunt) {
+
+    }
+
+    @Override
+    public void visit(PaintingsEvent paintings) {
+
+    }
+
+    @Override
+    public void visit(HunterCard card) {
+
+    }
+
+    @Override
+    public void visit(ShamanCard card) {
+        player.increaseShamanStars(card.getStars());
+    }
+
+    @Override
+    public void visit(ArtistCard card) {
+
+    }
+
+    @Override
+    public void visit(BuilderCard card) {
+
+    }
+
+    @Override
+    public void visit(InventorCard card) {
+
+    }
+
+    @Override
+    public void visit(GathererCard card) {
+
+    }
+
+    @Override
+    public void visit(TribeCard card) {
+
+    }
 }
