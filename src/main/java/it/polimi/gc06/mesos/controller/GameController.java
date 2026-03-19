41f9feedb5795b4f669c0e38b1a5c509e368f540
Tommaso Salomoni <tommaso.salomoni@mail.polimi.it>
2026-03-19 16:04:02 +0100

    [Feature] Initialized GameController methods for game interactions

--- src/main/java/it/polimi/gc06/mesos/controller/GameController.java
+++ src/main/java/it/polimi/gc06/mesos/controller/GameController.java
@@ -1,7 +1,27 @@
 package it.polimi.gc06.mesos.controller;
 
 import it.polimi.gc06.mesos.model.GameModel;
+import it.polimi.gc06.mesos.model.gameBoard.TileSlot;
+import it.polimi.gc06.mesos.model.cards.TribeCard;
+import it.polimi.gc06.mesos.model.cards.buildings.BuildingCard;
 
 public class GameController {
     private GameModel model;
+
+    public GameController(GameModel model) {
+        this.model = model;
+    }
+
+    public GameModel getModel() {
+        return model;
+    }
+
+    public void handleTotemOfferTilePlacement(String playerNickname, TileSlot slot) {
+    }
+
+    public void handleCardPick(String playerNickname, TribeCard card) {
+    }
+
+    public void handleBuildingBuying(String playerNickname, BuildingCard building) {
+    }
 }
