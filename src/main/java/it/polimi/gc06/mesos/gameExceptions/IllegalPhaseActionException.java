b11f603fb3bc1e80878a5712d50a322ee93d810e
Manuel Ricciardi <manuelricciardi233@gmail.com>
2026-03-19 16:00:56 +0100

    [Exceptions] phase exception.

---
+++ src/main/java/it/polimi/gc06/mesos/gameExceptions/IllegalPhaseActionException.java
@@ -0,0 +1,7 @@
+package it.polimi.gc06.mesos.gameExceptions;
+
+public class IllegalPhaseActionException extends IllegalGameActionException{
+    public IllegalPhaseActionException(String message) {
+        super(message);
+    }
+}
