b81134ccdff7517eb08b0ba9fe69c8dac26a786f
Tommaso Salomoni <tommaso.salomoni@mail.polimi.it>
2026-03-22 12:31:16 +0100

    [Refactor] Update card handling methods in GameController to use TurnManager and improve error handling

--- src/main/java/it/polimi/gc06/mesos/controller/GameController.java
+++ src/main/java/it/polimi/gc06/mesos/controller/GameController.java
@@ -4,8 +4,10 @@ import it.polimi.gc06.mesos.gameExceptions.IllegalPhaseActionException;
 import it.polimi.gc06.mesos.model.GameModel;
 import it.polimi.gc06.mesos.model.cards.TribeCard;
 import it.polimi.gc06.mesos.model.cards.buildings.BuildingCard;
+import it.polimi.gc06.mesos.model.cards.characters.CharacterCard;
 import it.polimi.gc06.mesos.model.gameBoard.TileSlot;
 import it.polimi.gc06.mesos.model.gameTurnManager.OfferResolutionPhase;
+import it.polimi.gc06.mesos.model.gameTurnManager.TurnManager;
 
 public class GameController {
     private GameModel model;
@@ -27,34 +29,64 @@ public class GameController {
             // TODO : handle this
         }
 
+        TurnManager turnManager = model.getTurnManager();
+
         try {
-            model.getTurnManager().getPhase().placeTotem(model.getTurnManager(), model.getTurnManager().getActivePlayer(), tile);
+            turnManager.getPhase().placeTotem(turnManager, turnManager.getActivePlayer(), tile);
         } catch (IllegalPhaseActionException e) {
             System.out.println(e.getMessage()); // TODO : communicate the error to the view
         }
     }
 
-    public void handleCardPickTopRow(String playerNickname, TribeCard card) {
+    public void handleCardPickTopRow(String playerNickname, CharacterCard card) {
         if (!model.getTurnManager().getActivePlayer().getNickname().equals(playerNickname)) {
             // TODO : handle this
         }
-    }
 
-    public void handleCardPickBottomRow(String playerNickname, TribeCard card) {
-        if (!model.getTurnManager().getActivePlayer().getNickname().equals(playerNickname)) {
-            // TODO : handle this
+        TurnManager turnManager = model.getTurnManager();
+        
+        try {
+            turnManager.getPhase().pickCardFromTop(turnManager, turnManager.getActivePlayer(), card);
+        } catch (IllegalPhaseActionException e) {
+            System.out.println(e.getMessage()); // TODO : communicate the error to the view
         }
     }
 
-    public void handleBuildingBuyingTopRow(String playerNickname, BuildingCard card) {
+    public void handleCardPickTopRow(String playerNickname, BuildingCard card) {
         if (!model.getTurnManager().getActivePlayer().getNickname().equals(playerNickname)) {
             // TODO : handle this
         }
+
+        try {
+            // TODO : we need the phase method to pick a building card
+        } catch (IllegalPhaseActionException e) {
+            System.out.println(e.getMessage()); // TODO : communicate the error to the view
+        }
     }
 
-    public void handleBuildingBuyingBottomRow(String playerNickname, BuildingCard card) {
+    public void handleCardPickBottomRow(String playerNickname, CharacterCard card) {
         if (!model.getTurnManager().getActivePlayer().getNickname().equals(playerNickname)) {
             // TODO : handle this
+        }
+
+        TurnManager turnManager = model.getTurnManager();
+
+        try {
+            turnManager.getPhase().pickCardFromBottom(turnManager, turnManager.getActivePlayer(), card);
+        } catch (IllegalPhaseActionException e) {
+            System.out.println(e.getMessage()); // TODO : communicate the error to the view
+        }
+    }
+
+    public void handleCardPickBottomRow(String playerNickname, BuildingCard card) {
+        if (!model.getTurnManager().getActivePlayer().getNickname().equals(playerNickname)) {
+            // TODO : handle this
+        }
+
+        try {
+            // TODO : we need the phase method to pick a building card
+        } catch (IllegalPhaseActionException e) {
+            System.out.println(e.getMessage()); // TODO : communicate the error to the view
         }
     }
 }
