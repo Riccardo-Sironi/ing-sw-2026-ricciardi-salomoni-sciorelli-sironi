b2b73f069dda3993fdd7ad792ec684b8701aafa0
Filippo Sciorelli <10899733@polimi.it>
2026-03-26 19:19:13 +0100

    [Testing] PlayerTest creation

---
+++ src/test/java/it/polimi/gc06/mesos/model/PlayerTest.java
@@ -0,0 +1,21 @@
+package it.polimi.gc06.mesos.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getFoodTokens() {
+
+    }
+
+    @Test
+    void addFoodTokens() {
+    }
+
+    @Test
+    void removeFoodTokens() {
+    }
+}
