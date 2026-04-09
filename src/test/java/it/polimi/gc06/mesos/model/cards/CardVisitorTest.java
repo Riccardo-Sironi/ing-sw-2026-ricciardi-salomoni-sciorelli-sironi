d68cf8012e29970d032ae6703c9783aea5bf1d9e
Filippo Sciorelli <10899733@polimi.it>
2026-04-09 17:15:01 +0200

    [Testing] Refactor CardVisitor to simplify assertDoesNotThrow syntax

--- src/test/java/it/polimi/gc06/mesos/model/cards/CardVisitorTest.java
+++ src/test/java/it/polimi/gc06/mesos/model/cards/CardVisitorTest.java
@@ -17,7 +17,7 @@ class CardVisitorTest {
     void testCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
 
-        assertDoesNotThrow(() -> { visitor.visit(mock(Card.class)); },
+        assertDoesNotThrow(() -> visitor.visit(mock(Card.class)),
                 "No exception should be thrown when visiting a Card");
     }
 
@@ -25,7 +25,7 @@ class CardVisitorTest {
     void testTribeCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
 
-        assertDoesNotThrow(() -> {visitor.visit(mock(TribeCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(TribeCard.class)),
                 "No exception should be thrown when visiting a TribeCard");
     }
 
@@ -33,7 +33,7 @@ class CardVisitorTest {
     void testEventCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
 
-        assertDoesNotThrow(() -> {visitor.visit(mock(EventCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(EventCard.class)),
                 "No exception should be thrown when visiting a EventCard");
     }
 
@@ -41,14 +41,14 @@ class CardVisitorTest {
     void testCharacterCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
 
-        assertDoesNotThrow(() -> {visitor.visit(mock(CharacterCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(CharacterCard.class)),
                 "No exception should be thrown when visiting a CharacterCard");
     }
 
     @Test
     void testBuildingCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(BuildingCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(BuildingCard.class)),
                 "No exception should be thrown when visiting a BuildingCard");
     }
 
@@ -58,28 +58,28 @@ class CardVisitorTest {
     @Test
     void testRitualEventVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(RitualEvent.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(RitualEvent.class)),
                 "No exception should be thrown when visiting a RitualEvent");
     }
 
     @Test
     void testSustenanceEventVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(SustenanceEvent.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(SustenanceEvent.class)),
                 "No exception should be thrown when visiting a SustenanceEvent");
     }
 
     @Test
     void testHuntEventVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(HuntEvent.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(HuntEvent.class)),
                 "No exception should be thrown when visiting a HuntEvent");
     }
 
     @Test
     void testPaintingsEventVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(PaintingsEvent.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(PaintingsEvent.class)),
                 "No exception should be thrown when visiting a PaintingsEvent");
     }
 
@@ -89,42 +89,42 @@ class CardVisitorTest {
     @Test
     void testHunterCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(HunterCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(HunterCard.class)),
                 "No exception should be thrown when visiting a HunterCard");
     }
 
     @Test
     void testShamanCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(ShamanCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(ShamanCard.class)),
                 "No exception should be thrown when visiting a ShamanCard");
     }
 
     @Test
     void testArtistCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(ArtistCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(ArtistCard.class)),
                 "No exception should be thrown when visiting a ArtistCard");
     }
 
     @Test
     void testBuilderCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(BuilderCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(BuilderCard.class)),
                 "No exception should be thrown when visiting a BuilderCard");
     }
 
     @Test
     void testInvetorCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(InventorCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(InventorCard.class)),
                 "No exception should be thrown when visiting a InventorCard");
     }
 
     @Test
     void testGathererCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(GathererCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(GathererCard.class)),
                 "No exception should be thrown when visiting a GathererCard");
     }
 
@@ -134,28 +134,28 @@ class CardVisitorTest {
     @Test
     void testEndGameBuildingCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(EndGameBuildingCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(EndGameBuildingCard.class)),
                 "No exception should be thrown when visiting a EndGameBuildingCard");
     }
 
     @Test
     void testModifierBuildingCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(ModifierBuildingCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(ModifierBuildingCard.class)),
                 "No exception should be thrown when visiting a ModifierBuildingCard");
     }
 
     @Test
     void testObserverSetBuildingCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(ObserverSetBuildingCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(ObserverSetBuildingCard.class)),
                 "No exception should be thrown when visiting a ObserverSetBuildingCard");
     }
 
     @Test
     void testObserverPairBuildingCardVisitor() {
         CardVisitor visitor = new CardVisitor() {};
-        assertDoesNotThrow(() -> {visitor.visit(mock(ObserverPairBuildingCard.class));},
+        assertDoesNotThrow(() -> visitor.visit(mock(ObserverPairBuildingCard.class)),
                 "No exception should be thrown when visiting a ObserverPairBuildingCard");
     }
 }
