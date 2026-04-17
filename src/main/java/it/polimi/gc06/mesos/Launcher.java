78add83a22ccffeaba895954915e55ce39eb7250
Tommaso Salomoni <tommaso.salomoni@mail.polimi.it>
2026-04-17 11:53:59 +0200

    [Refactor] Temporally comment out HelloApplication, HelloController, and Launcher classes to understand better cover precision

--- src/main/java/it/polimi/gc06/mesos/Launcher.java
+++ src/main/java/it/polimi/gc06/mesos/Launcher.java
@@ -1,9 +1,9 @@
-package it.polimi.gc06.mesos;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
+//package it.polimi.gc06.mesos;
+//
+//import javafx.application.Application;
+//
+//public class Launcher {
+//    public static void main(String[] args) {
+//        Application.launch(HelloApplication.class, args);
+//    }
+//}
