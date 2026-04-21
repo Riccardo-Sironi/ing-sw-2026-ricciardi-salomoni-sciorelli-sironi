b3abe90f08089f32d7b79ef38dae6a63f3ceadd3
Tommaso Salomoni <tommaso.salomoni@mail.polimi.it>
2026-04-21 16:38:59 +0200

    [Feature] Implement CardControllerVisitor for handling card actions and extend CardVisitor with additional visit methods

---
+++ src/main/java/it/polimi/gc06/mesos/controller/CardControllerVisitor.java
@@ -0,0 +1,33 @@
+package it.polimi.gc06.mesos.controller;
+
+import it.polimi.gc06.mesos.gameExceptions.IllegalGameActionException;
+import it.polimi.gc06.mesos.model.GameModel;
+import it.polimi.gc06.mesos.model.Player;
+import it.polimi.gc06.mesos.model.cards.CardVisitor;
+import it.polimi.gc06.mesos.model.cards.TribeCard;
+import it.polimi.gc06.mesos.model.cards.characters.CharacterCard;
+import it.polimi.gc06.mesos.model.cards.events.EventCard;
+import it.polimi.gc06.mesos.model.gameTurnManager.TurnManager;
+
+public class CardControllerVisitor extends CardVisitor {
+    private final TurnManager turnManager;
+    private final Player activePlayer;
+    private final GameModel model;
+
+
+    public CardControllerVisitor(TurnManager turnManager, Player activePlayer, GameModel model) {
+        this.turnManager = turnManager;
+        this.activePlayer = activePlayer;
+        this.model = model;
+    }
+
+    @Override
+    public void visit(CharacterCard card) {
+        turnManager.getPhase().pickCardFromTop(turnManager, activePlayer, card, model.getBoard());
+    }
+
+    @Override
+    public void visit(EventCard card) {
+        throw new IllegalGameActionException("Event cards cannot be picked!");
+    }
+}
--- src/main/java/it/polimi/gc06/mesos/model/cards/CardVisitor.java
+++ src/main/java/it/polimi/gc06/mesos/model/cards/CardVisitor.java
@@ -5,25 +5,74 @@ import it.polimi.gc06.mesos.model.cards.characters.*;
 import it.polimi.gc06.mesos.model.cards.events.*;
 
 public abstract class CardVisitor {
+    public void visit(Card card) {
+    }
 
-    public void visit(Card card){}
-    public void visit(TribeCard card){}
-    public void visit(EventCard card){}
-    public void visit(CharacterCard card){}
-    public void visit(BuildingCard card){}
-    public void visit(RitualEvent ritual){}
-    public void visit(SustenanceEvent sustenance){}
-    public void visit(HuntEvent hunt){}
-    public void visit(PaintingsEvent paintings){}
-    public void visit(HunterCard card){}
-    public void visit(ShamanCard card){}
-    public void visit(ArtistCard card){}
-    public void visit(BuilderCard card){}
-    public void visit(InventorCard card){}
-    public void visit(GathererCard card){}
-    public void visit(EndGameBuildingCard building){}
-    public void visit(ModifierBuildingCard building){}
-    public void visit(ObserverSetBuildingCard building){}
-    public void visit(ObserverPairBuildingCard building){}
-    
+    public void visit(TribeCard card) {
+    }
+
+    public void visit(EventCard card) {
+    }
+
+    public void visit(CharacterCard card) {
+    }
+
+    public void visit(BuildingCard card) {
+    }
+
+    public void visit(RitualEvent ritual) {
+        visit((EventCard) ritual);
+    }
+
+    public void visit(SustenanceEvent sustenance) {
+        visit((EventCard) sustenance);
+    }
+
+    public void visit(HuntEvent hunt) {
+        visit((EventCard) hunt);
+    }
+
+    public void visit(PaintingsEvent paintings) {
+        visit((EventCard) paintings);
+    }
+
+    public void visit(HunterCard card) {
+        visit((CharacterCard) card);
+    }
+
+    public void visit(ShamanCard card) {
+        visit((CharacterCard) card);
+    }
+
+    public void visit(ArtistCard card) {
+        visit((CharacterCard) card);
+    }
+
+    public void visit(BuilderCard card) {
+        visit((CharacterCard) card);
+    }
+
+    public void visit(InventorCard card) {
+        visit((CharacterCard) card);
+    }
+
+    public void visit(GathererCard card) {
+        visit((CharacterCard) card);
+    }
+
+    public void visit(EndGameBuildingCard building) {
+        visit((BuildingCard) building);
+    }
+
+    public void visit(ModifierBuildingCard building) {
+        visit((BuildingCard) building);
+    }
+
+    public void visit(ObserverSetBuildingCard building) {
+        visit((BuildingCard) building);
+    }
+
+    public void visit(ObserverPairBuildingCard building) {
+        visit((BuildingCard) building);
+    }
 }
