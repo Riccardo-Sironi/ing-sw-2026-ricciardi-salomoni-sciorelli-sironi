180ca454cab8920c7f5d8d32b93e58783b204d5d
Riccardo Sironi <riccardo1.sironi@mail.polimi.it>
2026-05-06 13:08:54 +0200

    [TUI] Add TuiBoardRenderer for rendering card rows and offer tracks in the TUI

---
+++ src/main/java/it/polimi/gc06/mesos/view/tui/TuiBoardRenderer.java
@@ -0,0 +1,88 @@
+package it.polimi.gc06.mesos.view.tui;
+
+import it.polimi.gc06.mesos.model.cards.Card;
+import it.polimi.gc06.mesos.model.gameBoard.TileSlot;
+import org.jline.terminal.Terminal;
+
+import java.util.List;
+
+public class TuiBoardRenderer {
+    private static final int CARD_HEIGHT = 7;
+    private static final String GAP_SEPARATOR = " ";
+    private final Terminal terminal;
+
+    public TuiBoardRenderer(Terminal terminal) {
+        this.terminal = terminal;
+    }
+
+    public void printCardRow(List<Card> rowCards) {
+        if (rowCards == null || rowCards.isEmpty()) {
+            terminal.writer().println("[Empty Row]");
+            return;
+        }
+
+        // StringBuilder is a mutable sequence of characters
+        // This allows us to append the cards one after another without creating a new string every time.
+        StringBuilder[] rowLines = new StringBuilder[CARD_HEIGHT];
+        for (int i = 0; i < CARD_HEIGHT; i++) {
+            rowLines[i] = new StringBuilder();
+        }
+        // We only need one visitor, and call visit every time we want to render a new card
+        TuiCardRendererVisitor renderer = new TuiCardRendererVisitor();
+
+        for (Card card : rowCards) {
+
+            String[] renderedCard = renderer.render(card);
+
+            // Append the card, and put a separator at the end
+            for (int i = 0; i < CARD_HEIGHT; i++) {
+                rowLines[i].append(renderedCard[i]).append(GAP_SEPARATOR);
+            }
+        }
+        
+        String[] centeredText = TuiView.centerOnScreen(rowLines, terminal);
+
+        for (String line : centeredText) {
+            terminal.writer().println(line);
+        }
+
+
+        terminal.writer().flush();
+    }
+
+    public void printOfferTrack(List<TileSlot> tiles) {
+        if (tiles == null || tiles.isEmpty()) {
+            terminal.writer().println("[Empty Row]");
+            return;
+        }
+
+        // StringBuilder is a mutable sequence of characters
+        // This allows us to append the cards one after another without creating a new string every time.
+        StringBuilder[] rowLines = new StringBuilder[CARD_HEIGHT];
+        for (int i = 0; i < CARD_HEIGHT; i++) {
+            rowLines[i] = new StringBuilder();
+        }
+        // We only need one visitor, and call visit every time we want to render a new card
+        TuiTileSlotRendererVisitor renderer = new TuiTileSlotRendererVisitor();
+
+        for (TileSlot tile : tiles) {
+            String[] renderedCard = renderer.render(tile);
+
+            // Append the card, and put a separator at the end
+            for (int i = 0; i < CARD_HEIGHT; i++) {
+                rowLines[i].append(renderedCard[i]).append(GAP_SEPARATOR);
+            }
+        }
+
+
+        String[] centeredText = TuiView.centerOnScreen(rowLines, terminal);
+
+        for (String line : centeredText) {
+            terminal.writer().println(line);
+        }
+
+        terminal.writer().flush();
+    }
+
+
+}
