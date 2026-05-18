da2003455710699f600efabf70bb341f2012e69c
Tommaso Salomoni <tommaso.salomoni@mail.polimi.it>
2026-05-18 17:34:32 +0200

    [Fix/TotemMovedDTO] improve null checks in edit method

--- src/main/java/it/polimi/gc06/mesos/dtos/TotemMovedDTO.java
+++ src/main/java/it/polimi/gc06/mesos/dtos/TotemMovedDTO.java
@@ -3,7 +3,7 @@ package it.polimi.gc06.mesos.dtos;
 import it.polimi.gc06.mesos.view.smallModel.PlayerView;
 import it.polimi.gc06.mesos.view.smallModel.SmallModel;
 
-public class TotemMovedDTO implements SmallModelEditor{
+public class TotemMovedDTO implements SmallModelEditor {
 
     private final String player;
     private final int index;
@@ -16,17 +16,16 @@ public class TotemMovedDTO implements SmallModelEditor{
     @Override
     public void edit(SmallModel smallModel) {
         PlayerView view;
-        if(smallModel.getPlayer().getNickname().equals(player)) {
+        if (smallModel.getPlayer().getNickname().equals(player)) {
             view = smallModel.getPlayer();
-        }
-        else{
+        } else {
             view = smallModel.getOpponents().stream().filter(v -> v.getNickname().equals(player))
                     .findFirst().orElseThrow(IllegalStateException::new);
         }
         //finds old pos and removes
-        for(int i = 0; i < smallModel.getTurnOrderTile().size(); i++ ){
-            if(smallModel.getTurnOrderTile().get(i).getNickname().equals(player)) {
-                smallModel.getTurnOrderTile().set(i,null);
+        for (int i = 0; i < smallModel.getTurnOrderTile().size(); i++) {
+            if (smallModel.getTurnOrderTile().get(i) == null || smallModel.getTurnOrderTile().get(i).getNickname().equals(player)) {
+                smallModel.getTurnOrderTile().set(i, null);
             }
         }
         //puts on new pos
