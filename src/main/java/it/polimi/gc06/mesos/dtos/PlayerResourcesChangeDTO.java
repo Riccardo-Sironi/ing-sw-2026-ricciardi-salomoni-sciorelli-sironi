da2e406ac5c678b40155fda0383ab4fd547cdd79
Tommaso Salomoni <tommaso.salomoni@mail.polimi.it>
2026-05-19 16:25:35 +0200

    [Fix/PlayerResourcesChangeDTO] change resource fields to Integer to handle null values in PlayerResourcesChangeDTO

--- src/main/java/it/polimi/gc06/mesos/dtos/PlayerResourcesChangeDTO.java
+++ src/main/java/it/polimi/gc06/mesos/dtos/PlayerResourcesChangeDTO.java
@@ -6,12 +6,12 @@ import it.polimi.gc06.mesos.view.smallModel.SmallModel;
 public class PlayerResourcesChangeDTO implements SmallModelEditor {
 
     private final String player;
-    private final int topDrawNum;
-    private final int bottomDrawNum;
-    private final int food;
-    private final int prestige;
+    private final Integer topDrawNum;
+    private final Integer bottomDrawNum;
+    private final Integer food;
+    private final Integer prestige;
 
-    public PlayerResourcesChangeDTO(String player, int topDrawNum, int bottomDrawNum, int food, int prestige) {
+    public PlayerResourcesChangeDTO(String player, Integer topDrawNum, Integer bottomDrawNum, Integer food, Integer prestige) {
         this.topDrawNum = topDrawNum;
         this.bottomDrawNum = bottomDrawNum;
         this.food = food;
@@ -22,16 +22,15 @@ public class PlayerResourcesChangeDTO implements SmallModelEditor {
     @Override
     public void edit(SmallModel smallModel) {
         if (smallModel.getPlayer().getNickname().equals(player)) {
-            if (food != 0) smallModel.getPlayer().setNumFood(food);
-            if (prestige != 0)
-                smallModel.getPlayer().setNumPrestige(prestige);
-            if (bottomDrawNum != 0) smallModel.setBottomDrawNum(bottomDrawNum);
-            if (topDrawNum != 0) smallModel.setTopDrawNum(topDrawNum);
+            if (food != null) smallModel.getPlayer().setNumFood(food);
+            if (prestige != null) smallModel.getPlayer().setNumPrestige(prestige);
+            if (bottomDrawNum != null) smallModel.setBottomDrawNum(bottomDrawNum);
+            if (topDrawNum != null) smallModel.setTopDrawNum(topDrawNum);
         } else {
             PlayerView view = smallModel.getOpponents().stream().filter(v -> v.getNickname().equals(player))
                     .findFirst().orElseThrow(IllegalStateException::new);
-            if (food != 0) view.setNumFood(food);
-            if (prestige != 0) view.setNumPrestige(prestige);
+            if (food != null) view.setNumFood(food);
+            if (prestige != null) view.setNumPrestige(prestige);
         }
     }
 
