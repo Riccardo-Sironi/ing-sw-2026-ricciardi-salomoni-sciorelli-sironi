cc2beb77ab1e20cbd69c60841395f8c6873eb5b0
Riccardo Sironi <riccardo1.sironi@mail.polimi.it>
2026-06-02 09:31:20 +0200

    [TUI] Add colors to the player info box

--- src/main/java/it/polimi/gc06/mesos/view/tui/TuiPlayerInfoRenderer.java
+++ src/main/java/it/polimi/gc06/mesos/view/tui/TuiPlayerInfoRenderer.java
@@ -2,6 +2,8 @@ package it.polimi.gc06.mesos.view.tui;
 
 import it.polimi.gc06.mesos.view.smallModel.PlayerView;
 
+import static it.polimi.gc06.mesos.view.tui.Style.getAnsiFromColorName;
+
 /**
  * A renderer responsible for transforming a player's state (infos such as the number of tokens, food, icons, characters etc.)
  * into a structured ASCII box to be rendered in the Text-based User Interface.
@@ -20,14 +22,16 @@ public class TuiPlayerInfoRenderer implements TuiRenderer<PlayerView> {
     public String[] render(PlayerView player) {
         String[] card = new String[7];
 
-        card[0] = "┌────────────────────┐";
+        String playerColor = getAnsiFromColorName(player.getColor().toString());
+
+        card[0] = playerColor + "┌────────────────────┐" + Style.RESET;
         // Center the player nickname, within 20 characters (22 - 2 for the borders)
-        card[1] = "│" + centerText(player.getNickname(), 20) + "│";
-        card[2] = "│" + centerText("\uD83C\uDF56" + player.getNumFood(), 10) + centerText("\uD83E\uDD47" + player.getNumPrestige(), 10) + "│";
-        card[3] = String.format("│  ★ %-2d ⚒ -%-2d  ✋%-2d  │", player.getNumShamanStar(), player.getBuildersDiscount(), player.getCollectedIcons().size());
-        card[4] = String.format("│  \uD83E\uDD57%-2d  \uD83C\uDFF9%-2d  \uD83C\uDFD7\uFE0F%-2d  │", player.getNumGatherer(), player.getNumHunter(), player.getNumBuilders());
-        card[5] = String.format("│  \uD83E\uDDE9%-2d  \uD83E\uDDB4%-2d  \uD83C\uDFA8%-2d  │", player.getNumInventor(), player.getNumShaman(), player.getNumArtist());
-        card[6] = "└────────────────────┘";
+        card[1] = playerColor + "│" + Style.RESET + centerText(player.getNickname(), 20) + playerColor + "│" + Style.RESET;
+        card[2] = playerColor + "│" + Style.RESET + centerText("\uD83C\uDF56" + player.getNumFood(), 10) + centerText("\uD83E\uDD47" + player.getNumPrestige(), 10) + playerColor + "│" + Style.RESET;
+        card[3] = String.format(playerColor + "│  " + Style.RESET + "★ %-2d ⚒ -%-2d  ✋%-2d  " + playerColor + "│" + Style.RESET, player.getNumShamanStar(), player.getBuildersDiscount(), player.getCollectedIcons().size());
+        card[4] = String.format(playerColor + "│  " + Style.RESET + "\uD83E\uDD57%-2d  \uD83C\uDFF9%-2d  \uD83C\uDFD7\uFE0F%-2d  " + playerColor + "│" + Style.RESET, player.getNumGatherer(), player.getNumHunter(), player.getNumBuilders());
+        card[5] = String.format(playerColor + "│  " + Style.RESET + "\uD83E\uDDE9%-2d  \uD83E\uDDB4%-2d  \uD83C\uDFA8%-2d  " + playerColor + "│" + Style.RESET, player.getNumInventor(), player.getNumShaman(), player.getNumArtist());
+        card[6] = playerColor + "└────────────────────┘" + Style.RESET;
 
         return card;
     }
