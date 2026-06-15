ebe6de8c1bba87a1f58f5149555f34adb8b16f22
Tommaso Salomoni <tommaso.salomoni@mail.polimi.it>
2026-06-15 11:28:57 +0200

    [Fix] fix error handling in PersistenceService

--- src/main/java/it/polimi/gc06/mesos/network/server/persistenceService/PersistenceService.java
+++ src/main/java/it/polimi/gc06/mesos/network/server/persistenceService/PersistenceService.java
@@ -26,11 +26,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class PersistenceService implements Runnable{
+public class PersistenceService implements Runnable {
 
     public static final String backupFileName = "matchBackup.json";
     public static final String tempFileName = "matchBackup.json.tmp";
-    /**In seconds*/
+    /**
+     * In seconds
+     */
     public static final long backupCooldown = 60;
     private final File backupFile;
     private final File tmpFile;
@@ -39,35 +41,44 @@ public class PersistenceService implements Runnable{
 
     public PersistenceService() throws IOException {
         //creates matchBackup.json if not present
-        try{
+        try {
             Files.createDirectories(ServerMain.appDirectory);
             Files.createFile(ServerMain.appDirectory.resolve(backupFileName));
             Files.createFile(ServerMain.appDirectory.resolve(tempFileName));
-        }catch (FileAlreadyExistsException _){}
+        } catch (FileAlreadyExistsException _) {
+        }
 
         this.backupFile = new File(ServerMain.appDirectory.resolve(backupFileName).toUri());
         this.tmpFile = new File(ServerMain.appDirectory.resolve(tempFileName).toUri());
         this.backupMatches = new HashMap<>();
         this.matchManager = null;
 
-        try{
+        try {
             //fill temp map
             ObjectMapper mapper = new ObjectMapper();
-            Map<Integer,GameSnapshot> snapshotMap = mapper.readValue(backupFile, new TypeReference<Map<Integer,GameSnapshot>>() {});
+
+            Map<Integer, GameSnapshot> snapshotMap = new HashMap<>();
+            if (backupFile.length() > 0) {
+                snapshotMap = mapper.readValue(backupFile, new TypeReference<Map<Integer, GameSnapshot>>() {
+                });
+            }
+
             //creates registry
             ModifierBuildingsRegistry registry = new ModifierBuildingsRegistry();
             InputStream input = PersistenceService.class.getResourceAsStream(ModelInstancesManager.JSON_PATH + "modifierCards.json");
             List<ModifierBuildingCard> modifierCards = mapper.readValue(input, new TypeReference<List<ModifierBuildingCard>>() {
             });
             modifierCards.forEach(registry::register);
+
             //fill backupMatches
             for (Map.Entry<Integer, GameSnapshot> entry : snapshotMap.entrySet()) {
-                backupMatches.put(entry.getKey(), RestoredMatch.restoreGame(entry.getValue(),registry));
+                backupMatches.put(entry.getKey(), RestoredMatch.restoreGame(entry.getValue(), registry));
             }
-        }catch (MismatchedInputException e){
-            //TODO: activate this error message only if the file wasn't empty
-            System.err.println("[PersistenceService] Error while trying to load from backup file...");
-            e.printStackTrace();
+        } catch (MismatchedInputException e) {
+            if (backupFile.length() > 0) {
+                System.err.println("[PersistenceService] Error while trying to load from backup file. The file might be corrupted.");
+                e.printStackTrace();
+            }
         }
     }
 
@@ -76,7 +87,7 @@ public class PersistenceService implements Runnable{
      *
      * @return 0 if the file is empty, otherwise the biggest match id saved.
      */
-    public int getBiggestBackupMatchId(){
+    public int getBiggestBackupMatchId() {
         return backupMatches.keySet().stream().mapToInt(Integer::intValue).max().orElse(0);
     }
 
@@ -85,8 +96,8 @@ public class PersistenceService implements Runnable{
      *
      * @throws IllegalStateException if the matchManager has not been provided.
      */
-    public void restoreMatches() throws IllegalStateException{
-        if(matchManager == null) throw new IllegalStateException("Match manager not set.");
+    public void restoreMatches() throws IllegalStateException {
+        if (matchManager == null) throw new IllegalStateException("Match manager not set.");
         backupMatches.forEach(matchManager::restoreMatch);
     }
 
@@ -111,18 +122,18 @@ public class PersistenceService implements Runnable{
                 Map<Integer, GameSnapshot> matchMap = new HashMap<>();
                 for (Match m : matchManager.getActiveMatches()) {
                     GameSnapshot gs = m.getSnapshot();
-                    if(gs != null) matchMap.put(m.getMatchId(), gs);
+                    if (gs != null) matchMap.put(m.getMatchId(), gs);
                 }
-                if(!matchMap.isEmpty()) {
+                if (!matchMap.isEmpty()) {
                     //first writes to tmp file than ask SO to move all content to backupFile to avoid corruption due to crash
                     mapper.writeValue(tmpFile, matchMap);
                     Files.move(tmpFile.toPath(), backupFile.toPath(), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
-                    System.out.println("Persistence service saved matches successfully ("+matchMap.keySet().stream().map(i -> String.valueOf(i))
-                            .collect(Collectors.joining(", "))+")");
+                    System.out.println("Persistence service saved matches successfully (" + matchMap.keySet().stream().map(i -> String.valueOf(i))
+                            .collect(Collectors.joining(", ")) + ")");
                 }
             }
-        } catch (InterruptedException _) {}
-        catch(IOException e){
+        } catch (InterruptedException _) {
+        } catch (IOException e) {
             System.err.println("PersistenceService failed: ");
             e.printStackTrace();
         }
